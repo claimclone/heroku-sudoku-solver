af718e903226671c5bfd838a58012696d67078b0
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-06-16 18:38:30 -0400

    row placement & functional testing

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -4,19 +4,42 @@ class SudokuSolver {
     const puzzleFormat = /(\d|\.){81}/;
     let result;
     if(puzzleString.length != 81){
-      result = 'invalid_length';
-      return result;
-    }
-    if(puzzleString.match(puzzleFormat)){
+      result = 'invalid length';
+    } else if(puzzleString.match(puzzleFormat)){
       result = 'valid';
     } else {  
-      result = 'invalid_char';
+      result = 'invalid char';
     }
     return result;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
+    //validate puzzle string
+    if(this.validate(puzzleString) != 'valid'){
+      return this.validate(puzzleString);
+    }
+    //check every placement in the row for the value
+    //if it exists, invalid placement
+    const RowsEnum = Object.freeze({"A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7, "I":8 });
+    //validate row, column, and value
+    const rowFormat = /[A-I]/i;
 
+    if(!row.toUpperCase().match(rowFormat) || !(1 >= column <= 9)){
+      return 'invalid coordinate';
+    }
+    if(!(1 >= value <= 9)){
+      return 'invalid value';
+    }
+
+    //go to start of a row and check if value exists in any placement
+    const rowStart = RowsEnum[row.toUpperCase()] * 9;
+    const rowEnd = rowStart + 9;
+    for(let i = rowStart; i < rowEnd; i++){
+      if(puzzleString[i] == value){
+        return 'invalid placement';
+      }
+    }
+    return 'valid placement';
   }
 
   checkColPlacement(puzzleString, row, column, value) {
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -12,12 +12,21 @@ suite('UnitTests', () => {
         });
     
         test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', function(){
-            assert.equal(solver.validate('1.5..2.84..63.12.7.2..5abcde9..1....8.2.3674.3.7.2..9.47...8..1..16..z.926914.37.'), 'invalid_char');
+            assert.equal(solver.validate('1.5..2.84..63.12.7.2..5abcde9..1....8.2.3674.3.7.2..9.47...8..1..16..z.926914.37.'), 'invalid char');
         });
     
         test('Logic handles a puzzle string that is not 81 characters in length', function(){
-            assert.equal(solver.validate('1.5..2.84.6....926914.37.'), 'invalid_length');
+            assert.equal(solver.validate('1.5..2.84.6....926914.37.'), 'invalid length');
         });
     });
+
+    suite('Logic for row placement', function(){
+        test('Logic handles a valid row placement', function(){
+            assert.equal(solver.checkRowPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.','I',4,5), 'valid placement');
+        })
+        test('Logic handles an invalid row placement', function(){
+            assert.equal(solver.checkRowPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.','A',1,4), 'invalid placement');
+        })
+    })
     
 });
