cf5783112ff9b3e3c0a0663cc83673a2e076f4ec
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-06-16 17:03:32 -0400

    sudoku solver validate function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,18 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    const puzzleFormat = /(\d|\.){81}/;
+    let result;
+    if(puzzleString.length != 81){
+      result = 'invalid_length';
+      return result;
+    }
+    if(puzzleString.match(puzzleFormat)){
+      result = 'valid';
+    } else {  
+      result = 'invalid_char';
+    }
+    return result;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
